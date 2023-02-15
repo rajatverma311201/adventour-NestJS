d4806c24a1bb9a08baab27bc727c703b22edbfc6
Rajat Verma <rajat311201@gmail.com>
2023-02-15 21:24:59 +0530

    added swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Adventour')
+    .setDescription(
+      'Adevntour REST API, Rebuilt using Nest-JS, Initial was using NodeJS, ExpressJS',
+    )
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
